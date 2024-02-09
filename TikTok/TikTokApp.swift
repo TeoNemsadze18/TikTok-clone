1a2ae3f0aec5f86d462160b08e8571e527507785
Teona Nemsadze <teona.nemsadze.2@iliauni.edu.ge>
2024-02-09 16:31:32 +0400

    Initial Commit

---
+++ TikTok/TikTokApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikTokApp.swift
+//  TikTok
+//
+//  Created by teona nemsadze on 09.02.24.
+//
+
+import SwiftUI
+
+@main
+struct TikTokApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
